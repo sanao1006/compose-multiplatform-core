cb9c36006b1061c180137bb1e0f36051d5b6c360
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-30 19:18:43 +0400

    UIKit demo, add real device target

--- compose/mpp/demo/build.gradle.kts
+++ compose/mpp/demo/build.gradle.kts
@@ -75,34 +75,8 @@ kotlin {
             }
         }
     }
-    iosX64("uikitX64") {
-        binaries {
-            executable() {
-                entryPoint = "androidx.compose.mpp.demo.main"
-                freeCompilerArgs += listOf(
-                    "-linker-option", "-framework", "-linker-option", "Metal",
-                    "-linker-option", "-framework", "-linker-option", "CoreText",
-                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
-                )
-                // TODO: the current compose binary surprises LLVM, so disable checks for now.
-                freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
-            }
-        }
-    }
-    iosSimulatorArm64("uikitSimArm64") {
-        binaries {
-            executable() {
-                entryPoint = "androidx.compose.mpp.demo.main"
-                freeCompilerArgs += listOf(
-                    "-linker-option", "-framework", "-linker-option", "Metal",
-                    "-linker-option", "-framework", "-linker-option", "CoreText",
-                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
-                )
-                // TODO: the current compose binary surprises LLVM, so disable checks for now.
-                freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
-            }
-        }
-    }
+    ios("uikit")
+    iosSimulatorArm64("uikitSimulatorArm64")
     cocoapods {
         version = "0.0.0"
         summary = "Shared code for the UIKit demo"
@@ -147,9 +121,9 @@ kotlin {
         val macosMain by creating { dependsOn(darwinMain) }
         val macosX64Main by getting { dependsOn(macosMain) }
         val macosArm64Main by getting { dependsOn(macosMain) }
-        val uikitMain by creating { dependsOn(darwinMain) }
-        val uikitX64Main by getting { dependsOn(uikitMain) }
-        val uikitArm64Main by creating { dependsOn(uikitMain) }
-        val uikitSimArm64Main by getting { dependsOn(uikitMain) }
+        val uikitMain by getting { dependsOn(darwinMain) }
+        val uikitSimulatorArm64Main by getting {
+            dependsOn(uikitMain)
+        }
     }
 }
